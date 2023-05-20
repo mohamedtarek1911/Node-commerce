2b2fcee4a097157c5218e7a82cdb9fd57582b49d
mohamedtarek1911 <mohamedtarek7670@gmail.com>
2023-05-21 00:05:07 +0200

    third commit

--- server.js
+++ server.js
@@ -16,17 +16,6 @@ const { webhookCheckout } = require("./controllers/orderService");
 
 const dbConnection = require("./config/database");
 
-// const categoryRouter = require('./routes/categoryRoute');
-// const subCategoryRouter = require('./routes/subCategoryRoute');
-// const brandRouter = require('./routes/brandRoute');
-// const productRouter = require('./routes/productRoute');
-// const userRouter = require('./routes/userRoute');
-// const authRouter = require('./routes/authRoute');
-// const reviewRouter = require('./routes/reviewRoute');
-// const wishlistRouter = require('./routes/wishlistRoute');
-// const addressRouter = require('./routes/addressRoute');
-// const couponRouter = require('./routes/couponRoute');
-
 // DB Connection
 dbConnection();
 
@@ -40,15 +29,12 @@ app.enable("trust proxy");
 // Add hook here before we call body parser, because stripe will send data in the body in form raw
 app.post(
   "/webhook-checkout",
-  // express.raw({ type: 'application/json' }),
   bodyParser.raw({ type: "application/json" }),
   webhookCheckout
 );
 
 // Used to parse JSON bodies
 app.use(express.json());
-// app.use(cors());
-// app.options('*', cors());
 
 // Parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
@@ -65,16 +51,6 @@ app.use(cors());
 
 // Mount routers
 mountRoutes(app);
-// app.use('/api/v1/categories', categoryRouter);
-// app.use('/api/v1/subcategories', subCategoryRouter);
-// app.use('/api/v1/brands', brandRouter);
-// app.use('/api/v1/products', productRouter);
-// app.use('/api/v1/users', userRouter);
-// app.use('/api/v1/auth', authRouter);
-// app.use('/api/v1/reviews', reviewRouter);
-// app.use('/api/v1/wishlist', wishlistRouter);
-// app.use('/api/v1/addresses', addressRouter);
-// app.use('/api/v1/coupons', couponRouter);
 
 app.all("*", (req, res, next) => {
   // 3) Use a generic api error
@@ -85,7 +61,7 @@ app.all("*", (req, res, next) => {
 // 2) with refactoring
 app.use(globalError);
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT;
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`.green);
 });
